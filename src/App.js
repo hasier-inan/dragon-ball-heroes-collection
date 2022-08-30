2347cd2aa364bf9c2af37e6f11acf2c300231987
Hasier Iñan <hasiermetal@gmail.com>
2022-08-30 16:08:33 +0200

    Added Github Pages.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DragonBallHeroesCollection from "./dragon-ball-heroes-collection";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <DragonBallHeroesCollection />
+      </section>
+    );
+  }
+}
+
+export default App;
